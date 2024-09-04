f8f115babb39e7123db379b10ac55ca74ec1a922
Leonard Schmidt <leonard.sch@live.de>
2024-09-04 16:32:54 +0200

    Add terraform file for bucket creation

---
+++ 01_docker_terraform/terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.0.1"
+    }
+  }
+}
+
+provider "google" {
+  project = "de-playground-24"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "de-playground-24-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
